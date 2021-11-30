ea30f4d2f709df66883c77ffa74d75273a9d0e58
ismail harmanda <iharmanda@hotmail.com>
2021-11-30 11:14:11 +0300

    create a styled img with animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
 import StyledButton, {
   FancyButton,
   SubmitButton,
 } from "./components/Button/Button";
 import "./App.css";
+import { AnimatedLogo } from "./components/Button/Button.styles";
 
 function App() {
   return (
     <div className="App">
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <AnimatedLogo src={logo} />
       <StyledButton type="submit">Styled Button</StyledButton>
       <div>
         <br />
